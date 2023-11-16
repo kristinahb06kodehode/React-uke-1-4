2ee76df3455a2c69125bbde06ff367ef31fa5074
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-11-16 14:22:32 +0100

    Fixed path on import

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "../App.css";
+
+import NavBar from "../../Components/NavBar/NavBar";
+import { blogText } from "../../Components/BlogPost/BlogText";
+import BlogList from "../../Components/BlogPost/BlogList";
+import SearchBar from "../../Components/NavBar/SearchBar";
+import FooterSection from "../../Components/NavBar/Footer";
+
+const Home: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [filteredBlogText, setFilteredBlogText] = useState(blogText);
+
+  const handleSearch = (searchTerm: string) => {
+    try {
+      setLoading(true);
+
+      const filteredResults = blogText.filter((post) =>
+        (post.content + " " + post.additionalContent)
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+      );
+
+      setFilteredBlogText(filteredResults);
+    } catch (error) {
+      if (typeof error === "string") {
+        setError(error);
+      } else {
+        console.error(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <NavBar>
+        <SearchBar onSearch={handleSearch} />
+      </NavBar>
+      <BlogList blogText={filteredBlogText} loading={loading} error={error} />
+      <FooterSection />
+    </div>
+  );
+};
+
+export default Home;

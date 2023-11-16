817fdbaa40ba5c8344a49f2ac88a13db5e8f023d
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-11-16 14:27:39 +0100

    Changed function type

--- src/pages/notfound/NotFound.tsx
+++ src/pages/notfound/NotFound.tsx
@@ -1,10 +1,11 @@
-export function NotFound() {
+import React from "react";
+
+const NotFound: React.FC = () => {
   return (
     <div>
-      <h2>404 - Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
+      <h1>404 - Not Found</h1>
     </div>
   );
-}
+};
 
 export default NotFound;

ea5326b1c2c8bc2f7e514519bc7c17ed91af287d
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-11-16 14:05:48 +0100

    cleanup router definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-import {
-  BrowserRouter,
-  Route,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Landing from "./pages/landing/LandingPage";
 import Home from "./pages/home/Home";
 import About from "./pages/about/AboutPage";
 import NotFound from "./pages/notfound/NotFound";
+import Layout from "./pages/layout/Layout";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Router>
-        <Route path="/" Component={Landing} />
-        <Route path="/home" Component={Home} />
-        <Route path="/about" Component={About} />
-        <Route Component={NotFound} />
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 

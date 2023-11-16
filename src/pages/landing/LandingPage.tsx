a64f1bf2056774ed8576375a490f465e33dde277
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-11-16 16:46:22 +0100

    added classNames

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "./LandingPage.module.css";
 
 const Landing: React.FC = () => {
   return (
-    <div className="landing">
-      <h1>Welcome to the Tech Blog!</h1>
-      <Link to="/home">
-        <button className="cta.btn">Enter page!</button>
+    <div className={style.landing}>
+      <h1 className={style.h1}>This is Kitty Central!</h1>
+      <Link to="/home" className={style.enterBtn}>
+        <button className={style.enterBtn}>Please Enter</button>
       </Link>
     </div>
   );
